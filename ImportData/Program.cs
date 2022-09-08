bc969834ef0529a70db97a94c2c88b85ee7b0b88
Rugal Tu <john2653589@gmail.com>
2022-09-08 15:47:23 +0800

    加入專案檔案。

---
+++ ImportData/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text;
+
+var Domain = "http://60.249.7.166";
+
+var FilePath = "data.csv";
+
+var AllLine = File.ReadAllLines(FilePath);
+using var Client = new HttpClient();
+var Url = $"{Domain}/CRIMS/api/Auth/Register_Contractor";
+
+foreach (var Line in AllLine.Skip(1))
+{
+    var Cols = Line.Split(",");
+    var CompanyName = Cols[0];
+    var UserName = Cols[1];
+    var Password = "123456";
+    var StaffName = Cols[3];
+    var StaffGender = true;
+    var StaffPhone = Cols[5];
+    var Data = new
+    {
+        CompanyName,
+        StaffNo = UserName,
+        StaffTypeId = 1,
+        StaffName,
+        StaffPhone,
+        StaffGender,
+        Password = Password,
+        RePassword = Password
+    };
+
+    var JsonString = System.Text.Json.JsonSerializer.Serialize(Data);
+    var Content = new StringContent(JsonString, Encoding.UTF8, "application/json");
+    await Client.PostAsync(Url, Content);
+}
